bfa66063ecc8abf4c03a7574fa08bb0e66cc4dd6
Toby <toby@themightymo.com>
2013-09-26 23:23:57 -0500

    Simplified and updated code base.

--- advanced-custom-fields-login-helper.php
+++ advanced-custom-fields-login-helper.php
@@ -1,29 +1,13 @@
 <?php
 /*
-Plugin Name: The Mighty Mo's Lowrize Logins Helper!
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
-Version: The Plugin's Version Number, e.g.: 1.0
-Author: Name Of The Plugin Author
-Author URI: http://URI_Of_The_Plugin_Author
-License: A "Slug" license name e.g. GPL2
+Plugin Name: Advanced Custom Fields Login Helper
+Plugin URI: https://github.com/themightymo/advanced-custom-fields-login-helper
+Description: Allows you to use Advanced Custom Fields to manage user access to posts and pages.
+Version: 1.0
+Author: Toby Cryns
+Author URI: http://www.themightymo.com
+License: GPL2
 */
-
-
-
-function display_acf_data($fieldName) {
-	if (get_field($fieldName)) {
-		the_field($fieldName);
-	}
-}
-
-function display_acf_data2 ($fieldName, $message, $isNot) {
-	if (get_field($fieldName) && get_field($fieldName) != $isNot) { 
-		echo '<li>';
-		echo $message . ' ' . get_field($fieldName); 
-		echo '</li>';
-	}
-}
 
 
 function does_user_have_access($content) {
@@ -49,10 +33,12 @@ function does_user_have_access($content) {
 			echo 'You do not have access to this post.  Please let Toby know if you do, indeed, need access.' . edit_post_link('Edit', '', ' ');
 		}
 	} else {
+		// If no users have been selected on this page's ACF field, then display the content for admins and display an "error" message for other users.
 		if ( current_user_can( 'manage_options' ) || current_user_can( 'create_users' ) ) {
 			// Display the contents of the post to admins
 			display_all_acf_fields();
 		} else {
+			// Error message for non-admins.
 			echo 'You do not have access to this post.  Please let Toby know if you do, indeed, need access.' . edit_post_link('Edit', '', ' ');
 		}
 			
@@ -63,73 +49,9 @@ function does_user_have_access($content) {
 }
 
 
-function display_all_acf_fields() { ?>
-	
-	<?php if (get_field('website_url') != 'http://' || get_field('wordpress_admin_user') || get_field('wordpress_admin_password')) { ?>
+function display_all_acf_fields() { 
 
-        <h2>Website Info:</h2>
-        
-        <ul>
-            <?php display_acf_data2('website_url', 'Website URL:', 'http://'); ?>
-        </ul>
-        
-        <h2>WordPress Login Info:</h2>
-        <ul>
-            <?php display_acf_data2('wordpress_login_url', 'WordPress Login URL:','http://' ); ?>
-            <?php if (get_field('wordpress_admin_user')) { ?><li>WordPress Admin Username: <?php display_acf_data('wordpress_admin_user'); ?></li><?php } ?>
-            <?php if (get_field('wordpress_admin_password')) { ?><li>WordPress Admin Password: <?php display_acf_data('wordpress_admin_password'); ?></li><?php } ?>
-        </ul>
-    
-    <?php } ?>
-	
-    <?php if (get_field('ip_address') || get_field('cpanel_url') || get_field('cpanel_username') || get_field('cpanel_password') || get_field('ftp_hostname') || get_field('ftp_username') || get_field('ftp_password')) { ?>
-    
-        <h2>cPanel & FTP:</h2>
-        <ul>
-        <?php if (get_field('ip_address')) { ?><li>ip Address: <?php display_acf_data('ip_address'); ?></li><?php } ?>
-        <?php if (get_field('cpanel_url')) { ?><li>cPanel URL: <?php display_acf_data('cpanel_url'); ?></li><?php } ?>
-        <?php if (get_field('cpanel_username')) { ?><li>cPanel Username: <?php display_acf_data('cpanel_username'); ?></li><?php } ?>
-        <?php if (get_field('cpanel_password')) { ?><li>cPanel Password: <?php display_acf_data('cpanel_password'); ?></li><?php } ?>
-        <?php if (get_field('ftp_hostname')) { ?><li>FTP Hostname: <?php display_acf_data('ftp_hostname'); ?></li><?php } ?>
-        <?php if (get_field('ftp_username')) { ?><li>FTP Username: <?php display_acf_data('ftp_username'); ?></li><?php } ?>
-        <?php if (get_field('ftp_password')) { ?><li>FTP Password: <?php display_acf_data('ftp_password'); ?></li><?php } ?>
-        </ul>
-	
-    <?php } ?>
-    
-    <?php if (get_field('contact_information_url')!='https://www.lowrize.com/themightymo/') { ?>
-
-	<h2>Contact Info:</h2>
-	<ul>
-		<?php if (get_field('contact_information_url')!='https://www.lowrize.com/themightymo/') { ?><li>Contact Info: <br /><?php display_acf_data('contact_information_url'); ?></li><?php } ?>
-	</ul>
-	
-    <?php } ?>
-    
-    
-    <?php if (get_field('miscellaneous_info')) { ?>
-
-        <h2>Miscellaneous Info:</h2>
-        <ul>
-            <?php if (get_field('miscellaneous_info')) { ?><li><?php display_acf_data('miscellaneous_info'); ?></li><?php } ?>
-        </ul>
-    
-   <?php } ?>
-   
-    <?php if ( current_user_can( 'manage_options' ) || current_user_can( 'create_users' ) ) {
-		// Display the contents of the post to admins
-		?>
-        <h2>Secret Info:</h2>
-        <ul>
-            <?php if (get_field('secret_info')) { ?><li><?php display_acf_data('secret_info'); ?></li><?php } ?>
-        </ul>
-        <?php
-	} ?>
-    
-    
-    
-    
-		<?php 
+		// Display a list of users who can access the current page to site admins. 
         if (current_user_can( 'manage_options' ) || current_user_can( 'create_users' )) {
             echo '<pre>';
             echo 'The following users have access to this login info:<br />';
@@ -142,45 +64,28 @@ function display_all_acf_fields() { ?>
             }  
             echo '</pre>';
         }
-	
-	
-	 
-	
-	if(get_field('attachments'))
-	{
-		echo '<ul>';
-	 
-		while(has_sub_field('attachments'))
-		{
-			$attachment = get_sub_field('attachment');
-			echo '<li><a href="' . $attachment['url'] . '">' . $attachment['title'] . '</li>';
-		}
-	 
-		echo '</ul>';
-	}
-	 
-	
-	
-	
-	
-	
-	
+        
+        /*
+        
+        HERE IS WHERE YOU ADD YOUR ADVANCED CUSTOM FIELD THEME HOOKS SUCH AS get_field($var) and the_field($var)
+        
+ ___________________          _-_
+ \==============_=_/ ____.---'---`---.____
+             \_ \    \----._________.----/
+               \ \   /  /    `-_-'
+           __,--`.`-'..'-_
+          /____          ||
+               `--.____,-'
+                
+		*/
+        
 }
 
-/*function my_custom_content_filter ($content) {
-	$content = $content . display_all_acf_fields();
-}*/
 add_filter('the_content', 'does_user_have_access', 20);
 
 
 
-
-
-
-
-
-
-// Fix the styling of the "users" Advanced Custom Field group
+// By default, the Advanced Custom Fields "User" field is super short.  This fixes that via CSS.
 function my_enqueue($hook) {
     if( 'post.php' != $hook )
     return;
